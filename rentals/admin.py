ccb0a486551286d27b7bf97db91caf8bdd7878e1
souleymanediallo <souleymanedialloo@yahoo.fr>
2024-01-27 16:38:51 +0100

    add models rental

--- rentals/admin.py
+++ rentals/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Rental
 # Register your models here.
+
+
+class RentalAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'book', 'rent_start_date', 'rent_end_date', 'created', 'updated')
+
+
+admin.site.register(Rental, RentalAdmin)
--- rentals/models.py
+++ rentals/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from books.models import Book
+from customers.models import Customer
+from datetime import timedelta
+
 
 # Create your models here.
+class Rental(models.Model):
+    STATUS_CHOICES = (
+        ('#01', 'rented'),
+        ('#02', 'returned'),
+        ('#03', 'lost'),
+        ('#04', 'delayed'),
+    )
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_rentals')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer_rentals')
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='#01')
+    rent_start_date = models.DateField()
+    rent_end_date = models.DateField(blank=True, null=True)
+    return_date = models.DateField(blank=True, null=True)
+    is_closed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.book.book_id} rented by {self.customer}"
+
+    def save(self, *args, **kwargs):
+        if not self.rent_end_date:
+            self.rent_end_date = self.rent_start_date + timedelta(days=14)
